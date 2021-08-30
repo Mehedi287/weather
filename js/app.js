ab195d179800f2e0768a5f8d0f9a65e9fa9010c6
Mehedi390 <mh0816150@gmail.com>
2021-08-30 08:25:45 -0700

    throw alrt

--- js/app.js
+++ js/app.js
@@ -5,20 +5,22 @@ const temp = document.getElementById('temp')
 const clouds = document.getElementById('clouds')
 weather.addEventListener('click', function () {
     const InputCityName = inputCity.value;
+    inputCity.value = '';
     if (InputCityName == '') {
         alert("Plasr Inter a Valide City Name")
     }
-    inputCity.value = '';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q= ${InputCityName}&appid=c8f177b8dffdbcf3b3b2b2229a78f046&units=metric`)
-        .then(res => res.json())
-        .then(data => {
-            if (data.cod == 404) {
-                alert("plase inter a valide city name")
-            }
-            else {
-                temp.innerText = data.main.temp;
-                clouds.innerText = data.clouds.all;
-                cityName.innerText = InputCityName;
-            }
-        })
+    else {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q= ${InputCityName}&appid=c8f177b8dffdbcf3b3b2b2229a78f046&units=metric`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.cod == 404) {
+                    alert("plase inter a valide city name")
+                }
+                else {
+                    temp.innerText = data.main.temp;
+                    clouds.innerText = data.clouds.all;
+                    cityName.innerText = InputCityName;
+                }
+            })
+    }
 })
